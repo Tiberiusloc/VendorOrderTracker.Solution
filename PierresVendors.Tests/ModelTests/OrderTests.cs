114cf5cf3bf5394ab3a2a3d99c0a7f438efa18ef
Tiberius Lockett <tlockett680@gmail.com>
2022-09-30 11:52:06 -0700

    add passing test 2 for ordertests.cs and passing test 1 for vendortests.cs

--- PierresVendors.Tests/ModelTests/OrderTests.cs
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -18,8 +18,26 @@ namespace PierresVendors.Tests
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
       DateTime date = new DateTime(2022, 07, 24);
-      Order newOrder = new Order("Chocoalte", 4, "Donut Order", date);
+      Order newOrder = new Order("Chocolate", 4, "Donut Order", date);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+        [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      //Arrange
+      string description = "Donut";
+      double price = 4;
+      string title = "Donut Order";
+      DateTime date = new DateTime(2022, 07, 24);
+      Order newOrder = new Order(description, price, title, date);
+
+      //Act
+      string updatedDescription = "Do the dishes";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(updatedDescription, result);
     }
   }
 }
---
+++ PierresVendors.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresVendors.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PierresVendors.Tests
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+    
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+    {
+      Vendor newVendor = new Vendor("Suzie", "Suzie's Donuts");
+      Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+  }
+}
