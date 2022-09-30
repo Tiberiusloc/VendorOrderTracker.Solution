a993f2413ea7e66a2c2efcfe9f1260608bbf9725
Tiberius Lockett <tlockett680@gmail.com>
2022-09-30 13:34:10 -0700

    Remove duplicate test, refactor test 2 add passing test 4 for ordertests.cs

--- PierresVendors.Tests/ModelTests/OrderTests.cs
+++ PierresVendors.Tests/ModelTests/OrderTests.cs
@@ -17,18 +17,34 @@ namespace PierresVendors.Tests
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_Order()
     {
-      DateTime date = new DateTime(2022, 07, 24);
+      DateTime date = new DateTime(1999, 01, 05);
       Order newOrder = new Order("Chocolate", 4, "Donut Order", date);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
-        [TestMethod]
+    [TestMethod]
+    public void GetDescription_ReturnsDescription_String()
+    {
+      //Arrange
+      string title = "Donut Order";
+      string description = "Donut";
+      double price = 4;
+      DateTime date = new DateTime(2022, 07, 24);
+
+      //Act
+      Order newOrder = new Order(description, price, title, date);
+      string result = newOrder.Description;
+
+      //Assert
+      Assert.AreEqual(description, result);
+    }
+    [TestMethod]
     public void SetDescription_SetDescription_String()
     {
       //Arrange
       string description = "Donut";
       double price = 4;
       string title = "Donut Order";
-      DateTime date = new DateTime(2022, 07, 24);
+      DateTime date = new DateTime(1999, 01, 05);
       Order newOrder = new Order(description, price, title, date);
 
       //Act
@@ -38,6 +54,18 @@ namespace PierresVendors.Tests
 
       //Assert
       Assert.AreEqual(updatedDescription, result);
+    }    
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      // Arrange
+      List<Order> newList = new List<Order> { };
+
+      // Act
+      List<Order> result = Order.GetAll();
+
+      // Assert
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 }
